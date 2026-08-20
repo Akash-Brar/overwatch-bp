e758d972bfd5b04a74d2a26db344c46d3bf7c50e
Akash-Brar <akashbrar467@gmail.com>
2026-08-20 10:42:42 -0600

    feat: implemented get_info_in_cell, which gets hero name, item name, and item type, from a cell.

--- bpScraperV2.py
+++ bpScraperV2.py
@@ -51,11 +51,11 @@ def get_item_types():
         a_tag = li.find("a")
         if a_tag and a_tag.has_attr("title"):
             if a_tag["title"] not in ["Player Titles", "Souvenirs", "Weapon Charms", "Weapon Variants"]:
-                item_types.append(a_tag["title"])
+                item_types.append(a_tag["title"][:-1])
 
     return item_types
 
-def get_2022_to_2026_bp_items(bp_data):
+def get_2022_to_2026_bp_items(heros_data):
     response = requests.get(PRE_2026_BP_URL)
 
     if response.status_code != 200:
@@ -65,13 +65,40 @@ def get_2022_to_2026_bp_items(bp_data):
     bp_tables = soup.find_all("table", class_="wikitable")
 
     for table in bp_tables:
-        table = table.find("tbody")
-        season = table.find_all("th")[0].text.strip()
-        
-  
+        table = table.find("tbody").find_all("tr")
+        season = table[0].text.strip()
+
+        rows = table[2:]
+        for row in rows:
+            cells = row.find_all("td")[1:]
+            free_item = get_info_in_cell(cells[0])
+            paid_item = get_info_in_cell(cells[1])
+
+            print(free_item)
+            print()
+            print(paid_item)
+            print()
+            break
+        break
+
+
+def get_info_in_cell(itemCell):
+    item = []
+    itemLinks = itemCell.find_all("a")
+    for link in itemLinks:
+        if link.has_attr("href") and "Cosmetic" in link["href"]:
+            hero_name = re.search(r'/w/(.*?)/Cosmetics', link["href"]).group(1)
+            item_name = link.text.strip().replace(f" - {hero_name}", "")
+            item_type = item_name.split(" ")[1]
+            item.append({
+                "hero": hero_name,
+                "item_name": item_name,
+                "item_type": item_type
+            })
+    return item
 
 if __name__ == "__main__":
-    # item_types = get_item_types()
-    # get_2022_to_2026_bp_items()
     heros = get_heros()
-    print(heros)
+    # item_types = get_item_types()
+    # print(item_types)
+    get_2022_to_2026_bp_items(heros)
